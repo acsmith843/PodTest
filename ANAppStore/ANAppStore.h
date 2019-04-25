a29bf1c08c3e45bd1855fff69eaca696f4ab0bbf
Adam C. Smith <smitha26@autonation.com>
2019-04-25 09:47:59 -0400

    Initial Commit

---
+++ ANAppStore/ANAppStore.h
@@ -0,0 +1,19 @@
+//
+//  ANAppStore.h
+//  ANAppStore
+//
+//  Created by Adam C. Smith on 4/25/19.
+//  Copyright © 2019 Adam C. Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ANAppStore.
+FOUNDATION_EXPORT double ANAppStoreVersionNumber;
+
+//! Project version string for ANAppStore.
+FOUNDATION_EXPORT const unsigned char ANAppStoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ANAppStore/PublicHeader.h>
+
+
