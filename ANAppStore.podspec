834e16bae78c46dc2349d88c3f1a0a49cb6a5be8
Adam C. Smith <smitha26@autonation.com>
2019-04-25 10:50:12 -0400

    Initial commit

---
+++ ANAppStore.podspec
@@ -0,0 +1,29 @@
+Pod::Spec.new do |s|
+
+s.platform = :ios
+s.ios.deployment_target = '12.0'
+s.name = "ANAppStore"
+s.summary = "A short description of ANAppStore."
+s.requires_arc = true
+
+s.version = "0.0.1"
+
+s.license = { :type => "MIT", :file => "LICENSE" }
+
+s.author = { "Adam C. Smith" => "smitha26@autonation.com" }
+
+s.homepage = 'https://github.com/acsmith843/AutoNationAppStore'
+
+s.source = { :git => 'https://github.com/acsmith843/AutoNationAppStore.git', :tag => s.version.to_s }
+
+s.framework = "UIKit"
+s.dependency 'Alamofire', '~> 4.7'
+s.dependency 'MBProgressHUD', '~> 1.1.0'
+
+s.source_files = "ANAppStore/**/*.{swift}"
+
+s.resources = "ANAppStore/**/*.{png,jpeg,jpg,storyboard,xib,xcassets}"
+
+s.swift_version = "4.2"
+
+end
---
+++ ANAppStore/services/AppStoreService.swift
@@ -0,0 +1,42 @@
+//
+//  AppStoreService.swift
+//  ANAppStore
+//
+//  Created by Adam C. Smith on 4/25/19.
+//  Copyright © 2019 Adam C. Smith. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class AppStoreService {
+    
+    typealias APICallCompletion = (_ success: Bool, _ responseObject: Any?) -> Void
+
+    static let shared = AppStoreService()
+    
+    func checkVersion(completionHandler: @escaping APICallCompletion) {
+        
+        let params: Parameters = [ "TODO:": "" ]
+        
+        let appStoreUrl = "/TODO"
+        
+        Alamofier.request(appStoreUrl,
+                          method: .get,
+                          parameters: params,
+                          encoding: URLEncoding.default,
+                          headers: nil)
+            .validate()
+            .responseJSON { response in
+                
+                switch response.result {
+                case .success(let value):
+                    // TODO:
+                    completionHandler(true, value)
+                case .failure(let error):
+                    print("Error during checkVersion.")
+                    completionHandler(false, response.response?.statusCode)
+                }
+        }
+    }
+}
